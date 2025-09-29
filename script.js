ffc5fbda8231d784e15581b3b0afcef41cabda34
Estebancpuen <esteban.cifuentes@uao.edu.co>
2025-09-29 12:23:55 -0500

    s

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// Acordeón
 const headers = document.querySelectorAll(".accordion-header");
 
 headers.forEach(header => {
     header.addEventListener("click", () => {
         const content = header.nextElementSibling;
-        content.style.maxHeight = content.style.maxHeight ? null : content.scrollHeight + "px";
+        content.classList.toggle("open");
     });
 });
