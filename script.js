c6919601fc82179030419530e7f7ff0f3d58396f
Estebancpuen <esteban.cifuentes@uao.edu.co>
2025-09-29 12:00:44 -0500

    ss

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function saludar() {
-    alert("¡Hola desde JavaScript!");
-}
+const headers = document.querySelectorAll(".accordion-header");
+
+headers.forEach(header => {
+    header.addEventListener("click", () => {
+        const content = header.nextElementSibling;
+        content.style.maxHeight = content.style.maxHeight ? null : content.scrollHeight + "px";
+    });
+});
